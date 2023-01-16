24bb435e90b5d84e17394ecd23a53307ce6fc241
Mariya <appleslover@yandex.ru>
2023-01-16 14:51:14 +0300

    Exampl 005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") {
+    Console.WriteLine("Ура, это же Маша!");
+}
+else {
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
