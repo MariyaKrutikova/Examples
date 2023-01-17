95e58d80b4931a68bd47f6c3817bb3cb78995934
Mariya <appleslover@yandex.ru>
2023-01-17 15:35:04 +0300

    Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {11, 72, 23, 4, 45, 16, 87, 68};
+int n = array.Length;
+int index = 0;
+int find = 45;
+
+while (index < n) 
+{
+    if (array[index] == find) 
+        {
+            Console.WriteLine(index);
+            break; 
+        }
+               index++;
+ } 
+
+
+
